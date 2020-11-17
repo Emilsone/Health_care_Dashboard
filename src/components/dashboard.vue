045c92c75e2bd7bebe9b410fefded3c2114a2ff1
Emilsone <lawsonezekiel68@gmail.com>
2020-11-17 16:10:17 +0100

    create sidebar and topnav

---
+++ src/components/dashboard.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <header>
+      <!-- <h1>USER DASHBOARD</h1> -->
+      <TopNav />
+    </header>
+    <div>
+      <div class="sidebar">
+        <SideBar />
+      </div>
+      <div class="main-page relative">
+        <router-view></router-view>
+        
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import SideBar from "./side-bar/side-bar.vue";
+import TopNav from "./top-nav/top-nav.vue";
+export default {
+  name: "Dashboard",
+  components: { SideBar,TopNav },
+};
+</script>
+
+<style scoped>
+ /* main > header {
+  width: 100%;
+  height: 7%;
+} */
+
+/* main {
+  height: 100vh;
+}  */
+
+/* main > div {
+  height: 93%;
+  display: flex;
+} */
+
+</style>
---
+++ src/components/top-nav/top-nav.vue
@@ -0,0 +1,121 @@
+<template>
+  <section>
+    <!-- component -->
+    <nav class="flex items-center justify-between flex-wrap  p-6">
+      <div class="flex items-center flex-no-shrink text-white mr-6">
+        <!-- component -->
+        <div class="relative text-gray-600">
+          <input
+            type="search"
+            name="serch"
+            placeholder="Search"
+            class="bg-gray-300 h-10 px-5 pr-10 rounded text-sm focus:outline-none"
+          />
+          <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+            <svg
+              class="h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              style="enable-background:new 0 0 56.966 56.966;"
+              xml:space="preserve"
+              width="512px"
+              height="512px"
+            >
+              <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          class="flex items-center 	px-3 py-2 border rounded text-teal-lighter border-teal-light hover:border-white"
+        >
+          <svg
+            class="h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-md lg:flex-grow lg:mx-32">
+          <a
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter mr-12"
+          >
+            Health Status
+          </a>
+          <a
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter mr-12"
+          >
+            Devices
+          </a>
+          <a
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter mr-12"
+          >
+            Medical Records
+          </a>
+          <a
+            href="#responsive-header"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter mr-12"
+          >
+            Health Management
+          </a>
+        </div>
+        <div class="flex mx-6">
+          <a href="#">
+            <span>
+              <svg
+                class="fill-current h-6 w-6 text-black  hover:text-green-500 mr-6"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M14 3V3.28988C16.8915 4.15043 19 6.82898 19 10V17H20V19H4V17H5V10C5 6.82898 7.10851 4.15043 10 3.28988V3C10 1.89543 10.8954 1 12 1C13.1046 1 14 1.89543 14 3ZM7 17H17V10C17 7.23858 14.7614 5 12 5C9.23858 5 7 7.23858 7 10V17ZM14 21V20H10V21C10 22.1046 10.8954 23 12 23C13.1046 23 14 22.1046 14 21Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </span>
+          </a>
+          <a href="#">
+            <span>
+              <svg
+                class="fill-current h-6 w-6 text-black  hover:text-green-500 mr-6"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M14 3V3.28988C16.8915 4.15043 19 6.82898 19 10V17H20V19H4V17H5V10C5 6.82898 7.10851 4.15043 10 3.28988V3C10 1.89543 10.8954 1 12 1C13.1046 1 14 1.89543 14 3ZM7 17H17V10C17 7.23858 14.7614 5 12 5C9.23858 5 7 7.23858 7 10V17ZM14 21V20H10V21C10 22.1046 10.8954 23 12 23C13.1046 23 14 22.1046 14 21Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  </section>
+</template>
+<script>
+export default {
+  name: "top-nav",
+};
+</script>
